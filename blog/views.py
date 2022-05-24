50984a42a20fdd5d67627b60f391f661e48fba0c
Vsirotkin <vsirotkin15@gmail.com>
2022-05-24 13:42:50 +0300

    first commit

---
+++ blog/views.py
@@ -0,0 +1,7 @@
+from django.views.generic import ListView
+from .models import *
+
+# Create your views here.
+class BlogListView(models.ListView):
+    model = Post
+    template_name = 'home.html'
