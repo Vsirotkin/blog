a509e87528643c78610bf9845cac52f847090a52
Vsirotkin <vsirotkin15@gmail.com>
2022-05-25 21:21:37 +0300

    setup test for user

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+
+from .models import *
 
 # Create your tests here.
+class BlogTest(TestCase):
+
+    def setUp(self):
+        self.user = get_user_model().objects.create_user (
+            username = 'testuser',
+            email = 'test@email.local',
+            password = 'secret',
+        )
+
+        self.post = Post.objects.create (
+            title = 'a goot title',
+            body = 'nice body content',
+            author = self.user,
+        )
+        
