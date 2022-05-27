2500dccc8855992e8ae93d3c7e4ead9f10eb667f
Vsirotkin <vsirotkin15@gmail.com>
2022-05-27 11:30:36 +0300

    set tests for update, delete

--- blog/tests.py
+++ blog/tests.py
@@ -26,6 +26,10 @@ class BlogTest(TestCase):
         self.assertEqual(str(post), post.title)
     
 
+    def test_get_absolute_url(self):
+        self.assertEqual(self.post.get_absolute_url(), '/post/1/')
+    
+
     def test_post_content(self):
         self.assertEqual(f'{self.post.title}', 'a good title')
         self.assertEqual(f'{self.post.author}', 'testuser')
@@ -46,3 +50,16 @@ class BlogTest(TestCase):
         self.assertEqual(no_response.status_code, 404)
         self.assertContains(response, 'a good title')
         self.assertTemplateUsed(response, 'post_detail.html')
+
+
+    def test_post_update_view(self):
+        response = self.client.post(reverse('post_edit', args='1'), {
+            'title': 'Update title',
+            'body': 'Update text',
+        })
+        self.assertEqual(response.status_code, 302)
+    
+
+    def test_post_delete_view(self):
+        response = self.client.post(reverse('post_delete', args='1'))
+        self.assertEqual(response.status_code, 302)
